a041c1308cc7a1ffd22684b262992f9a0d066e44
István Földy <37741458+stevefoldy@users.noreply.github.com>
2020-12-03 20:42:46 +0100

    first commit, index.php added

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Newsletter Form</title>
+</head>
+<body>
+
+<section class="newsletter-section">
+    <div class="container">
+        <div class="newsletter-form-title">
+            Hírlevél
+        </div>
+        <div class="newsletter-form-title-after">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae lacus neque. Nullam pretium mollis lectus, vel dictum ante congue in. In at elementum ex.
+        </div>
+        <form class="newsletter-form row">
+            <div class="form-group col-md-4">
+                <input type="text" class="form-control" placeholder="Vezetéknév" id="exampleInputPassword1" required>
+            </div>
+            <div class="form-group col-md-4">
+                <input type="email" class="form-control" placeholder="Email cím" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+            </div>
+            <div class="form-group col-12 col-md">
+                <button type="submit" class="btn btn-primary w-100">Feliratkozom</button>
+            </div>
+            <div class="form-group form-check col-12 ml-3 mx-md-4 mt-2">
+                <input type="checkbox" class="form-check-input custom-control-input" id="exampleCheck1">
+                <label class="form-check-label custom-control-label" for="exampleCheck1">Hozzájárulok ahhoz, hogy a NewsletterTeam a nevemet és e-mail címemet hírlevelezési céllal kezelje és a részemre gazdasági reklámot is tartalmazó email hírleveleket küldjön.</label>
+            </div>
+        </form>
+    </div>
+</section>
+    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
